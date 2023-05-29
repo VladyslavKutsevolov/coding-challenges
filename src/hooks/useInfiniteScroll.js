b9343f7106f0eb6ef4e3cda08ac48312b64fa589
Vlad <kytsevolov@gmail.com>
2023-05-29 08:53:28 -0600

    updated fetch url

--- src/hooks/useInfiniteScroll.js
+++ src/hooks/useInfiniteScroll.js
@@ -16,7 +16,7 @@ const UseInfiniteScroll = (users, setUsers) => {
         clearTimeout(loadMoreTimeoutRef.current);
 
         loadMoreTimeoutRef.current = setTimeout(async () => {
-          const getUsers = await fetch("http://localhost:3000/api/user/getUsers", {
+          const getUsers = await fetch("http://127.0.0.1:3000/api/user/getUsers", {
               method: "GET",
           });
 
